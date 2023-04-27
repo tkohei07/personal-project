f49ff3e74befbbd9c017e11ff4e02fadd6a8b235
tkohei07 <koheitagai.kyotouni@gmail.com>
2023-04-26 22:57:28 -0500

    add var file

---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "db_username" {
+  type        = string
+}
+
+variable "db_password" {
+  type        = string
+}
+
+variable "db_name" {
+  type        = string
+}
+
+variable "dsn" {
+  type        = string
+}
+
+variable "aws_arn" {
+  type        = string
+}
+
+variable "task_arn" {
+  type        = string
+}
+
+variable "service_arn" {
+  type        = string
+}
+
+variable "rds_arn" {
+  type        = string
+}
